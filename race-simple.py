beb780b9c791ec413e125f2b482d86fc745b69fe
gael-roustan-epsi-2022 <98700023+gael-roustan-epsi-2022@users.noreply.github.com>
2023-01-11 14:29:12 +0000

    new skeleton method instance for track class

--- race-simple.py
+++ race-simple.py
@@ -1,19 +1,39 @@
 class Track():
-	pass
+
+    def add_space_ship(self, space_ship):
+        pass  # todo
+
+    def start_race(self):
+        pass  # todo
+
+    def who_wins(self):
+        pass  # todo
+
 
 class SpaceShip():
-	pass
+
+    def __init__(self, name, speed):
+        self._name = name
+        self._speed = speed
+
+    def get_name(self):
+        return self._name
+
+    def __repr__(self):
+        return f'{self._name}: {self._speed} km/h'
 
 
-track1 = Track(2000)
-spaceShip1 = SpaceShip("space1", 5) # km/h
+track1 = Track()  # todo
+spaceShip1 = SpaceShip("space1", 5)  # km/h
 spaceShip2 = SpaceShip("space2", 6)
 spaceShip3 = SpaceShip("space3", 3)
 spaceShip4 = SpaceShip("space4", 8)
 spaceShip5 = SpaceShip("space5", 2)
 
-track1.addSpaceShip(spaceShip1) # do the same for remaining spaceship
+print(spaceShip1)
 
-track1.startRace()
+track1.add_space_ship(spaceShip1)  # do the same for remaining spaceship
 
-print(track1.whoWins()) # display space4
+track1.start_race()
+
+print(track1.who_wins())  # display space4
